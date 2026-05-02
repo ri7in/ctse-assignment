c9c317f59d30858e6d8f84a3c643f52692718909
Thusala Piyarisi <thusalapi@gmail.com>
2026-05-02 14:05:12 +0530

    style: Standardize quotes and add types to tsconfig.json

--- infrastructure/aws/bin/tasky.ts
+++ infrastructure/aws/bin/tasky.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { TaskyStack } from '../lib/tasky-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { TaskyStack } from "../lib/tasky-stack";
 
 const app = new cdk.App();
-new TaskyStack(app, 'TaskyStack', {
+new TaskyStack(app, "TaskyStack", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
+    region: process.env.CDK_DEFAULT_REGION || "us-east-1",
   },
 });
